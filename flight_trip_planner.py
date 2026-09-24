50bbebdb3ba846c5b114c9e8fc662a811a2db6f1
Ali Tarom <alifathitarom244@gmail.com>
2026-09-23 22:54:41 -0400

    Create basic Flight Trip Planner

---
+++ flight_trip_planner.py
@@ -0,0 +1,9 @@
+"""Flight Trip Planner
+
+Author: Ali Tarom
+Purpose: A simple program for planning a trip and calculating travel costs.
+Date: September 2026
+"""
+
+print("===== FLIGHT TRIP PLANNER =====")
+print("Plan your next trip!")
