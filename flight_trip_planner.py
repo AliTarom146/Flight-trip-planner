0678524c095c1287e0fe8a885c1c5efa6ab24e75
Ali Tarom <alifathitarom244@gmail.com>
2026-09-23 23:02:01 -0400

    Add trip information

--- flight_trip_planner.py
+++ flight_trip_planner.py
@@ -7,3 +7,17 @@ Date: September 2026
 
 print("===== FLIGHT TRIP PLANNER =====")
 print("Plan your next trip!")
+
+destination = "Paris"
+flight_cost = 850
+hotel_cost = 600
+nights = 4
+activities = ["Eiffel Tower", "Louvre Museum"]
+
+print()
+print("Trip Information")
+print("Destination:", destination)
+print("Flight Cost: $", flight_cost)
+print("Hotel Cost: $", hotel_cost)
+print("Nights:", nights)
+print("Activities:", activities)
